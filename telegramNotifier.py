9e5da2297022df30c46f7ec05dc1b922da6bbf13
Rex <rex@bitbeschleuniger.de>
2019-03-29 21:59:35 +0100

    fixed bug with media caption length

--- telegramNotifier.py
+++ telegramNotifier.py
@@ -14,5 +14,11 @@ class TelegramNotifier:
         self.bot = updater.bot
 
     def send_messages(self, url, preview_url, caption):
-        self.LOGGER.debug("Sending "+caption+" with url "+url)
-        self.bot.send_photo(chat_id=self.chat_id, photo=preview_url, caption=caption + " - "+url)
+
+        title = caption + " - " + url
+        if len(title) > 1024:
+            self.LOGGER.info("Title to long. Need to cut it")
+            max_length = 1023 - len("... - " + url)
+            title = caption[:max_length] + "... - " + url
+        self.LOGGER.debug("Sending "+title)
+        self.bot.send_photo(chat_id=self.chat_id, photo=preview_url, caption=title, disable_notification=False)
